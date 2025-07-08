573bf1d8f5696012a1811c6672de6e92a59defaa
Amad Fareed <amadfareed@gmail.com>
2025-07-08 14:37:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     DATABASE_URL = "mysql+pymysql://root:12345678@localhost:3306/minerva_db"
     
-    # This is the permanent directory for your documents
+    # This is the permanent directory for the university documents (slides)
     DOCUMENT_DIRECTORY = "B:\\Modules"
